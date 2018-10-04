480152726ceed30ada35d2d612a4d715995a6c6a
Simon Dupree <dupreenator@gmail.com>
2018-10-04 18:00:06 +0100

    postcodesIO api call on each postcodeArray element > relates #18

--- controllers/airtable.js
+++ controllers/airtable.js
@@ -1,4 +1,4 @@
-module.exports = (req, res) => {
+module.exports = (req, res, next) => {
     // set up airtable api
     var Airtable = require('airtable');
     Airtable.configure({
@@ -8,6 +8,12 @@ module.exports = (req, res) => {
     var base = Airtable.base('apphdQNWTLdRQbOOg');
     // set up storage array to be sent to frontend
     let storage = [];
+    // setup postcodesIO
+    var PostcodesIO = require('postcodesio-client');
+
+    var postcodes = new PostcodesIO('https://api.postcodes.io', {
+        headers: { 'User-Agent': 'MyAwesomeApp/1.0.0' } // default {} - extra headers
+    });
     // start query
     base('fonthilldummy').select({
         // Selecting the first 3 records in Grid view:
@@ -24,27 +30,35 @@ module.exports = (req, res) => {
             storage.push(postcodeIdObj)
         });
         // create postcode object to be sent to geolocation api
-        let postcodeObj = {
-            "postcodes": storage.map(postcode => {
-                return postcode.postcode
-            })
-        }
-        // let postcodeArr = storage.map(postcode => {
-        //     return postcode.postcode
-        // })
-
-        console.log(postcodeObj);
-
+        // let postcodeObj = {
+        //     "postcodes": storage.map(postcode => {
+        //         return postcode.postcode
+        //     })
+        // }
+        let postcodeArr = storage.map(postcode => {
+            return postcode.postcode
+        });
+        console.log(postcodeArr);
+        postcodeArr.forEach(postcode => {
+            postcodes
+                .lookup(postcode)
+                .then(function (postcode) {
+                    console.log(postcode);
+                }, function (error) {
+                    next(error);
+                });
+        })
 
         // To fetch the next page of records, call `fetchNextPage`.
         // If there are more records, `page` will get called again.
         // If there are no more records, `done` will get called.
         fetchNextPage();
+    },
 
-    }, function done(err) {
-        if (err) { console.error(err); return; }
-        res.send({ ...storage })
+        function done(err) {
+            if (err) { console.error(err); next(err); }
+            res.send({ ...storage })
 
-    });
+        });
 
 }
