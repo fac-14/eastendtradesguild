8ebe5ac6f20909473ef4ea2d3dde0e4578dc4fe7
Michael Watts <hello@michaelwatts.co>
2018-10-04 15:47:51 +0100

    Revert "update target for static routes"
    This reverts commit 58e0ebb58a073e63dd31828b21479e164e886503.

--- controllers/index.js
+++ controllers/index.js
@@ -4,15 +4,20 @@ const error = require('./error');
 
 const router = express.Router();
 
+// require different handlers from this folder
+// e.g. const landing = require("./landing");
+
 router.get('/api/hello', hello);
+
+console.log('Node ENV is: ', process.env.NODE_ENV);
 
 // if in production mode, serve React files from static folder client/build
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  router.use(express.static(path.join(__dirname, '..', 'client/build')));
+  router.use(express.static(path.join(__dirname, 'client/build')));
   // Handle React routing, return all requests to React app
   router.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', '..', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
