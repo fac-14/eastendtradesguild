05d4ce2d1e087e87a9f68ff8970ce707ec00a1e0
Michael Watts <hello@michaelwatts.co>
2018-10-04 15:37:15 +0100

    move static routes back to server.js

--- server.js
+++ server.js
@@ -6,4 +6,14 @@ const port = process.env.PORT || 5000;
 
 app.use(controllers);
 
+// if in production mode, serve React files from static folder client/build
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  router.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  router.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
