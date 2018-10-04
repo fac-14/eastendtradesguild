61497b7a37258f225392a20372f9c45dd47c315e
Simon Dupree <dupreenator@gmail.com>
2018-10-04 17:01:40 +0100

    airtable api postcode object request to be sent to geoloc api >relates #18

--- controllers/airtable.js
+++ controllers/airtable.js
@@ -1,39 +1,50 @@
-// const base = require('airtable').base('apphdQNWTLdRQbOOg');
-
-
-module.exports =  (req,res) => {
-
+module.exports = (req, res) => {
+    // set up airtable api
     var Airtable = require('airtable');
-Airtable.configure({
-    endpointUrl: 'https://api.airtable.com',
-    apiKey: 'keyYBKUirvMxeaey5'
-});
-var base = Airtable.base('apphdQNWTLdRQbOOg');
-let storage = [];
-
-base('fonthilldummy').select({
-    // Selecting the first 3 records in Grid view:
-    maxRecords: 3,
-    pageSize: 1,
-    view: "no_geolocation"
-}).eachPage(function page(records, fetchNextPage) {
-    // This function (`page`) will get called for each page of records.
-
-    records.forEach(function(record) {
-        // console.log('Retrieved', record.get('ID'));
-        storage.push(record)
+    Airtable.configure({
+        endpointUrl: 'https://api.airtable.com',
+        apiKey: 'keyYBKUirvMxeaey5'
     });
-    
+    var base = Airtable.base('apphdQNWTLdRQbOOg');
+    // set up storage array to be sent to frontend
+    let storage = [];
+    // start query
+    base('fonthilldummy').select({
+        // Selecting the first 3 records in Grid view:
+        maxRecords: 3,
+        pageSize: 3,
+        view: "no_geolocation"
+    }).eachPage(function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+        records.forEach(function (record) {
+            postcodeIdObj = {
+                id: record.id,
+                postcode: record.fields.Postcode
+            }
+            storage.push(postcodeIdObj)
+        });
+        // create postcode object to be sent to geolocation api
+        let postcodeObj = {
+            "postcodes": storage.map(postcode => {
+                return postcode.postcode
+            })
+        }
+        // let postcodeArr = storage.map(postcode => {
+        //     return postcode.postcode
+        // })
 
-    // To fetch the next page of records, call `fetchNextPage`.
-    // If there are more records, `page` will get called again.
-    // If there are no more records, `done` will get called.
-    fetchNextPage();
+        console.log(postcodeObj);
 
-}, function done(err) {
-    if (err) { console.error(err); return; }
-    res.send({...storage})
 
-});
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+
+    }, function done(err) {
+        if (err) { console.error(err); return; }
+        res.send({ ...storage })
+
+    });
 
 }
