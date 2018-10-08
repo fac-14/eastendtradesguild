fd3ddf0a0b2dd2a9bdc4327db8326cb2562b7e7b
Michael Watts <hello@michaelwatts.co>
2018-10-08 11:09:35 +0100

    initialise from arrested-developer/cra-plus-express relates #20

--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -1,12 +1,8 @@
 const express = require('express');
-
-// require in your controller files
 const hello = require('./hello');
 
-// create a new router instance
 const router = express();
 
-// define your routes
 router.get('/api/hello', hello);
 
 module.exports = router;
