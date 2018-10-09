3f8aab91c359aea98cb99264321238d3b065aecb
Simon Dupree <dupreenator@gmail.com>
2018-10-09 12:00:09 +0100

    refactorig postcodeIO api call and testing

--- src/server/models/postcodes.js
+++ src/server/models/postcodes.js
@@ -1,4 +1,15 @@
+const request = require('request');
 
-const makePostcodeArray = input => input.map(entry => entry.postcode)
+const makePostcodeArray = inputArray =>
+    inputArray.map(entry => entry.postcode)
 
-module.exports = { makePostcodeArray }
+const getGeolocation = array => {
+    //console.log('hello');
+    request.post('https://api.postcodes.io/postcodes', {
+        json: { "postcodes": array },
+    }, (error, res, body) => {
+        console.log('I\'m a callback')
+    })
+    //return [{ lat: 10, lng: 5 }, { lat: 108, lng: -180 }, { lat: 55, lng: 120 }];
+}
+module.exports = { makePostcodeArray, getGeolocation }
--- src/server/models/postcodes.test.js
+++ src/server/models/postcodes.test.js
@@ -1,4 +1,4 @@
-const { makePostcodeArray } = require('./postcodes');
+const { makePostcodeArray, getGeolocation } = require('./postcodes');
 
 const noGeoOutput = [
     { id: 'recavzIUC1T3IT1uO', postcode: 'N4 3HF' },
@@ -6,7 +6,19 @@ const noGeoOutput = [
     { id: 'rec6i8mI1ua3ZK6D9', postcode: 'N4 3HQ' }
 ]
 
+const postcodeArray = ['N4 3HF', 'N4 3HH', 'N4 3HQ'];
+
+const postcodeGeoData = require('../../../mocks/postcodes_geolocation_data.json')
+
 it('produces an array', () => {
     const actual = Array.isArray(makePostcodeArray(noGeoOutput));
     expect(actual).toBeTruthy();
+    expect(makePostcodeArray(noGeoOutput)).toEqual(postcodeArray);
+})
+
+it('takes an array of postcodes and sends back lat and long for each item', () => {
+    const actual = getGeolocation(postcodeArray);
+    expect(Array.isArray(actual)).toBeTruthy();
+    expect(actual.length).toBe(3);
+    expect(typeof actual[0]).toBe('object');
 })
