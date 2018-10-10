c556e3f378b471a536197695bb4c6e7621866f45
zeanort <armandwork@hotmail.co.uk>
2018-10-10 12:45:10 +0100

    Got leaflet map with icon and label to render

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
-
+import React, { Component } from "react";
+import "./App.css";
+import Map from "./Map";
 
 class App extends Component {
   state = {
-    response: '',
+    response: "",
     lat: 51.505,
     lng: -0.09,
     zoom: 13
@@ -34,20 +33,9 @@ class App extends Component {
   // };
 
   render() {
-    const position = [this.state.lat, this.state.lng]
     return (
       <div className="App">
-        <Map center={position} zoom={this.state.zoom}>
-          <TileLayer
-            attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={position}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-          </Marker>
-        </Map>
+        <Map />
       </div>
     );
   }
---
+++ src/components/Map.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+
+export default class LeafletMap extends Component {
+  state = {
+    lat: 51.505,
+    lng: -0.09,
+    zoom: 13
+  };
+
+  render() {
+    const position = [this.state.lat, this.state.lng];
+    return (
+      <Map center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+    );
+  }
+}
