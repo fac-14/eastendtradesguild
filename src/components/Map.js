f1e144cf11827469110d21ca6b20eed2b9f90006
zeanort <armandwork@hotmail.co.uk>
2018-10-10 13:14:29 +0100

    map rendering with multiple map points

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,26 +1,49 @@
 import React, { Component } from "react";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
-export default class LeafletMap extends Component {
+type Position = [number, number];
+
+type Props = {|
+  content: string,
+  position: Position
+|};
+
+type MarkerData = {| ...Props, key: string |};
+
+const MyPopupMarker = ({ content, position }: Props) => (
+  <Marker position={position}>
+    <Popup>{content}</Popup>
+  </Marker>
+);
+
+const MyMarkersList = ({ markers }: { markers: Array<MarkerData> }) => {
+  const items = markers.map(({ key, ...props }) => (
+    <MyPopupMarker key={key} {...props} />
+  ));
+  return <React.Fragment>{items}</React.Fragment>;
+};
+
+type State = {
+  markers: Array<MarkerData>
+};
+
+export default class CustomComponent extends Component<{}, State> {
   state = {
-    lat: 51.505,
-    lng: -0.09,
-    zoom: 13
+    markers: [
+      { key: "marker1", position: [51.5, -0.1], content: "My first popup" },
+      { key: "marker2", position: [51.51, -0.1], content: "My second popup" },
+      { key: "marker3", position: [51.49, -0.05], content: "My third popup" }
+    ]
   };
 
   render() {
-    const position = [this.state.lat, this.state.lng];
     return (
-      <Map center={position} zoom={this.state.zoom}>
+      <Map center={[51.505, -0.09]} zoom={13}>
         <TileLayer
           attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        <MyMarkersList markers={this.state.markers} />
       </Map>
     );
   }
