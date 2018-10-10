77cb4b1370d8d229c1fe9b1d06a0b1430c1641e5
Simon Dupree <dupreenator@gmail.com>
2018-10-10 12:33:05 +0100

    map component in app rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,53 @@
 import React, { Component } from 'react';
-import logo from './assets/logo.svg';
 import './App.css';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+
 
 class App extends Component {
   state = {
     response: '',
+    lat: 51.505,
+    lng: -0.09,
+    zoom: 13
   };
 
-  componentDidMount() {
-    this._mounted = true;
-    this.callApi()
-      .then(res => {
-        this.setState({ response: res.express });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+  // componentDidMount() {
+  //   this._mounted = true;
+  //   this.callApi()
+  //     .then(res => {
+  //       this.setState({ response: res.express });
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // }
 
-  componentWillUnmount() {
-    this._mounted = false;
-  }
+  // componentWillUnmount() {
+  //   this._mounted = false;
+  // }
 
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    return body;
-  };
+  // callApi = async () => {
+  //   const response = await fetch('/api/hello');
+  //   const body = await response.json();
+  //   if (response.status !== 200) throw Error(body.message);
+  //   return body;
+  // };
 
   render() {
+    const position = [this.state.lat, this.state.lng]
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.response}</p>
+        <Map center={position} zoom={this.state.zoom}>
+          <TileLayer
+            attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+          </Marker>
+        </Map>
       </div>
     );
   }
