7e5b96be19ad35e3c78a1cea69004e2146de0920
Simon Dupree <dupreenator@gmail.com>
2018-10-16 14:33:32 +0100

    refactored PostcodeForm and update state in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   state = {
     response: "",
     markers: [],
-    center: []
+    postcode: '',
+    center: [] || ''
   };
 
   componentDidMount() {
@@ -26,11 +27,39 @@ class App extends Component {
     return body;
   };
 
+
+  // FORM FUNCTIONS
+
+  handleChange = event => {
+    const value = event.target.value;
+    this.setState({ postcode: value })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const postcode = this.state.postcode;
+    this.apiCallGeo(postcode)
+  }
+
+  apiCallGeo = postcode => {
+    fetch(`https://api.postcodes.io/postcodes/${postcode}`)
+      .then(res => res.json())
+      .then(json => this.createLatLongArr(json))
+      .then(array => this.setState({ center: array }))
+  }
+
+  createLatLongArr = object => {
+    if (object.status === 404) {
+      return object.error
+    }
+    return [Object.values(object.result)[6], Object.values(object.result)[7]]
+  }
+
   render = () => {
     return (
       <div className="App">
         {/* <Map markers={this.state.markers} center={this.state.center} /> */}
-        <PostcodeForm geolocation={this.state.center} />
+        <PostcodeForm onSubmit={this.handleSubmit} postcode={this.state.postcode} center={this.state.center} onChange={this.handleChange} />
       </div>
     );
   }
--- src/components/PostcodeForm.js
+++ src/components/PostcodeForm.js
@@ -2,43 +2,11 @@ import React from 'react'
 
 export default class Form extends React.Component {
 
-    state = {
-        postcode: '',
-        geolocation: [] || ''
-    }
-
-    apiCallGeo = postcode => {
-        fetch(`https://api.postcodes.io/postcodes/${postcode}`)
-            .then(res => res.json())
-            .then(json => this.createLatLongArr(json))
-            .then(array => this.setState({ geolocation: array }))
-    }
-
-    createLatLongArr = object => {
-        if (object.status === 404) {
-            return object.error
-        }
-        return [Object.values(object.result)[6], Object.values(object.result)[7]]
-    }
-
-    handleChange = event => {
-        const value = event.target.value;
-        this.setState({ postcode: value })
-    }
-
-    handleSubmit = event => {
-        event.preventDefault();
-        const postcode = this.state.postcode;
-        this.apiCallGeo(postcode)
-        // .then(this.setState({ geolocation: }))
-
-    }
-
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={this.props.onSubmit}>
                 <label htmlFor='postcode'>Postcode:</label>
-                <input type='text' id='postcode' name='postcode' value={this.state.postcode} onChange={this.handleChange} />
+                <input type='text' id='postcode' name='postcode' value={this.props.postcode} onChange={this.props.onChange} />
                 <button type="submit">Submit</button>
             </form>
         )
