a3df2d7e5aba916f6d8806a3e901b095f5aad22d
Simon Dupree <dupreenator@gmail.com>
2018-10-18 13:57:04 +0100

    create conditional rendering for popup section for each marker > #relates 10

--- src/components/Map.js
+++ src/components/Map.js
@@ -9,26 +9,26 @@ import "./Map.css";
 
 type Props = {|
   name: string,
-  geolocation: string,
-  postcode: string,
-  address: string,
-  price_sqft: number,
-  use_class: string,
-  lease_length: string,
-  date_of_last_rent_review: number,
-  date_of_next_rent_review: number,
-  square_feet: number,
-  break_clauses: string,
-  useColor: object,
-  annual_rent: number,
-  yard_sqft: number,
-  yard_price_sqft: number,
-  restricted: string,
-  specification: string,
-  landlord_name: string,
-  additional_comments: string,
-  landlord_tenants_act: string
-|};
+    geolocation: string,
+      postcode: string,
+        address: string,
+          price_sqft: number,
+            use_class: string,
+              lease_length: string,
+                date_of_last_rent_review: number,
+                  date_of_next_rent_review: number,
+                    square_feet: number,
+                      break_clauses: string,
+                        useColor: object,
+                          annual_rent: number,
+                            yard_sqft: number,
+                              yard_price_sqft: number,
+                                restricted: string,
+                                  specification: string,
+                                    landlord_name: string,
+                                      additional_comments: string,
+                                        landlord_tenants_act: string
+                                          |};
 
 type MarkerData = {| ...Props, key: string |};
 
@@ -98,7 +98,7 @@ const MarkerWithPopup = ({
   landlord_tenants_act
 }: Props) => {
   const price = price_sqft.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
-
+  console.log(MarkerWithPopup)
   return (
     <Marker position={JSON.parse(geolocation)} icon={iconSelect(use_class)}>
       <Popup
@@ -108,50 +108,51 @@ const MarkerWithPopup = ({
         className={"popup"}
       >
         <div className="pa0 avenir f5 tl mw5">
-          <PopupLabel>Address:</PopupLabel>
+          {(address || postcode) &&
+            <PopupLabel>Address:</PopupLabel>}
           <PopupInfo>
             {address}, {postcode}
           </PopupInfo>
-
-          <PopupLabel>Landlord name</PopupLabel>
+          {(landlord_name) &&
+            <PopupLabel>Landlord name</PopupLabel>}
           <PopupInfo>{landlord_name}</PopupInfo>
-
-          <PopupLabel>Use Class: </PopupLabel>
+          {(use_class) &&
+            <PopupLabel>Use Class: </PopupLabel>}
           <PopupInfo>
             <Pill use_class={use_class}>{use_class}</Pill>
           </PopupInfo>
-
-          <PopupLabel>Square Feet</PopupLabel>
+          {(square_feet) &&
+            <PopupLabel>Square Feet</PopupLabel>}
           <PopupInfo>{square_feet}</PopupInfo>
-
-          <PopupLabel>Yard square feet</PopupLabel>
+          {(yard_sqft) &&
+            <PopupLabel>Yard square feet</PopupLabel>}
           <PopupInfo>{yard_sqft}</PopupInfo>
-
-          <PopupLabel>Yard price /sqft</PopupLabel>
+          {(yard_price_sqft) &&
+            <PopupLabel>Yard price /sqft</PopupLabel>}
           <PopupInfo>{yard_price_sqft}</PopupInfo>
-
-          <PopupLabel>Lease Length</PopupLabel>
+          {(lease_length) &&
+            <PopupLabel>Lease Length</PopupLabel>}
           <PopupInfo>{lease_length}</PopupInfo>
-
-          <PopupLabel>Last rent review</PopupLabel>
+          {(date_of_last_rent_review) &&
+            <PopupLabel>Last rent review</PopupLabel>}
           <PopupInfo>{date_of_last_rent_review}</PopupInfo>
-
-          <PopupLabel>Next rent review</PopupLabel>
+          {(date_of_next_rent_review) &&
+            <PopupLabel>Next rent review</PopupLabel>}
           <PopupInfo>{date_of_next_rent_review}</PopupInfo>
-
-          <PopupLabel>Landlord tennants act</PopupLabel>
+          {(landlord_tenants_act) &&
+            <PopupLabel>Landlord tennants act</PopupLabel>}
           <PopupInfo>{landlord_tenants_act}</PopupInfo>
-
-          <PopupLabel>Break Clause</PopupLabel>
+          {(break_clauses) &&
+            <PopupLabel>Break Clause</PopupLabel>}
           <PopupInfo>{break_clauses}</PopupInfo>
-
-          <PopupLabel>Restricted</PopupLabel>
+          {(restricted) &&
+            <PopupLabel>Restricted</PopupLabel>}
           <PopupInfo>{restricted}</PopupInfo>
-
-          <PopupLabel>Specification</PopupLabel>
+          {(specification) &&
+            <PopupLabel>Specification</PopupLabel>}
           <PopupInfo>{specification}</PopupInfo>
-
-          <PopupLabel>Additional comments</PopupLabel>
+          {(additional_comments) &&
+            <PopupLabel>Additional comments</PopupLabel>}
           <PopupInfo>{additional_comments}</PopupInfo>
 
           <CenteredSection>
@@ -187,10 +188,12 @@ const Markers = ({ markers }: { markers: Array<MarkerData> }) => {
   const items = markers.map(({ key, ...props }) => (
     <MarkerWithPopup key={key} {...props} />
   ));
+  console.log('markers', markers[0])
+  console.log('items', items)
   return <React.Fragment>{items}</React.Fragment>;
 };
 
-const createClusterCustomIcon = function(cluster) {
+const createClusterCustomIcon = function (cluster) {
   return L.divIcon({
     html: `<span>${cluster.getChildCount()}</span>`,
     className: "f6 link dim br-pill w2 h2 pt2 dib white bg-dark-pink tc b",
