cf731540c502a0bfae1ed83710e1f37fe38e18f4
Armand-Lluka <armandwork@hotmail.co.uk>
2018-10-19 02:56:12 +0100

    legend transition animation added

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 const colors = ['A1:#ff80cc', 'A3:#9eebcf', 'B1:#96ccff', 'B2:#fbf1a9', 'B8:#ffb700', 'D1:#a463f2', 'D2:#ff6300', 'other:#fff']
 
@@ -7,9 +7,9 @@ const createLegend = (open) => {
     if (open) {
         return colors.map(color =>
             (
-                <Circle color={color.split(':')[1]}>
+                <AnimateCircle color={color.split(':')[1]}>
                     <UseText>{color.split(':')[0]}</UseText>
-                </Circle>
+                </AnimateCircle>
             )
         )
     } return (
@@ -19,13 +19,37 @@ const createLegend = (open) => {
     )
 }
 
+
+
+  function rotationBuilder() {
+    const rotation = keyframes`
+    from {
+        opacity: 0;
+        -webkit-transform: translate3d(0, 40px, 0);
+        transform: translate3d(0, 40px, 0);
+      }
+    
+      to {
+        opacity: 1;
+        -webkit-transform: translate3d(0, 0, 0);
+        transform: translate3d(0, 0, 0);
+      }
+    `;
+    return rotation;
+  }
+
+
 const LegendUseclass = styled.div.attrs({
     className: "fixed left-1 bottom-1 z-max"
 })``;
 
 const Circle = styled.div.attrs({
     className: "mt2 w2 h2 br-100 flex items-center justify-center "
-})`background:${props => props.color}`
+})`background:${props => props.color }`
+
+const AnimateCircle = styled.div.attrs({
+    className: "mt2 w2 h2 br-100 flex items-center justify-center "
+})`background:${props => props.color};  animation: ${rotationBuilder()} 0.5s linear ; `
 
 const UseText = styled.p.attrs({
     className: "avenir black"
