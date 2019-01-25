338b3d83c1843837a43eb5dc54523ea8cb2e4774
Michael Watts <hello@michaelwatts.co>
2019-01-25 09:41:36 +0000

    put base id in env vars

--- src/server/models/getRecords.js
+++ src/server/models/getRecords.js
@@ -18,7 +18,7 @@ Airtable.configure({
   endpointUrl: 'https://api.airtable.com',
   apiKey,
 });
-const base = Airtable.base('appZ5iPToPB60Mo0F');
+const base = Airtable.base(process.env.AIRTABLE_BASE);
 
 // getRecords :: Contents:
 
